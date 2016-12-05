63e6055f23ddb73916e5fe1998fcd3589de1dc99
ewanzd <ewanzd@gmail.com>
2016-12-05 22:33:40 +0100

    Add doc and set standard game type

--- Stratego.Game/StrategoGame.cs
+++ Stratego.Game/StrategoGame.cs
@@ -18,43 +18,44 @@ namespace Stratego.Game
         public Guid GameId { get { return _gameId; } }
 
         /// <summary>
-        /// Game type.
+        /// Game type from this game.
         /// </summary>
         public IGameType GameType { get { return _type; } }
 
         /// <summary>
         /// Create a new classic stratego game with two players.
         /// </summary>
-        /// <param name="playerOne"></param>
-        /// <param name="playerTwo"></param>
+        /// <param name="playerOne"><see cref="System.Guid"/> of player one.</param>
+        /// <param name="playerTwo"><see cref="System.Guid"/> of player two.</param>
         public StrategoGame(Guid playerOne, Guid playerTwo) 
-            : this(new StrategoTypeClassic(), new Guid[] { playerOne, playerTwo })
+            : this(null, new Guid[] { playerOne, playerTwo })
         {
 
         }
 
         /// <summary>
-        /// 
+        /// Create new game with any type of game.
         /// </summary>
-        /// <param name="type"></param>
-        /// <param name="players"></param>
+        /// <param name="type">Type of game.</param>
+        /// <param name="players">All players <see cref="System.Guid"/>, who participate this game.</param>
         private StrategoGame(IGameType type, params Guid[] players)
         {
             if (players.Length != type.CountOfPlayer)
                 throw new ArgumentException("Count of players are not valid.");
 
+            _type = type ?? new StrategoTypeClassic();
             _gameId = Guid.NewGuid();
             _listOfPlayers = new ReadOnlyCollection<Guid>(players);
         }
 
         /// <summary>
-        /// 
+        /// Get player on position X (start on 1).
         /// </summary>
-        /// <param name="number"></param>
-        /// <returns></returns>
-        public Guid GetPlayer(int order)
+        /// <param name="pos">The position of the player.</param>
+        /// <returns><see cref="System.Guid"/> of player.</returns>
+        public Guid GetPlayer(int pos)
         {
-            return _listOfPlayers[order];
+            return _listOfPlayers[pos - 1];
         }
     }
 }
